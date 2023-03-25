1c9fb639054befc9c3605de517d03bedfe51a305
rubenleal89 <rubendarioleal618@gmail.com>
2023-03-25 16:55:10 -0500

    Data de registro guardada en indexedDB con validaciones

--- registroRuben.js
+++ registroRuben.js
@@ -17,16 +17,46 @@ function registroUser() {
             db = req.result
             console.log("Create",db);
             db.createObjectStore("User",{
-                // autoIncrement:true
                 keyPath:"User"
             })
         }
 
         // Agregar usuarios
-        const addData = (data)=>{
+        const addUser = (data)=>{
             const transaction = db.transaction(['User'],'readwrite')
             const objectStore = transaction.objectStore('User')
-            const req = objectStore.add(data)
+            objectStore.add(data)
+        }
+
+        const readData = (userInput)=>{
+            const transaction = db.transaction(['User'])
+            const objectStore = transaction.objectStore('User')
+            const req = objectStore.openCursor()
+            const labelUser = document.getElementById("labelUser")
+            const inputUser = document.getElementById("user")
+
+            req.onsuccess = (e)=>{
+                const cursor = e.target.result
+
+                if (cursor) {
+                    let userId = cursor.value.User
+                    cursor.continue()
+                    if (userId === userInput) {
+                        throw new Error(
+                            labelUser.className = "error",
+                            inputUser.placeholder = "El usuario ya existe",
+                            form.user.value = ""
+                        )
+                    }
+                    else {
+                        labelUser.className = ""
+                        inputUser.placeholder = "Usuario"
+                    }
+                }
+            }
+            req.onerror = (e) =>{
+                console.log(e);
+            }
         }
 
         req.onerror = (error)=>{
@@ -35,6 +65,12 @@ function registroUser() {
 
         // Evento del formulario
         form.addEventListener("submit",(e)=>{
+            let labelEmail = document.getElementById("labelEmail")
+            let labelConfEmail = document.getElementById("labelConfEmail")
+            let inputConfEmail = document.getElementById("confEmail")
+            let labelPassword = document.getElementById("labelPassword")
+            let labelConfPassword = document.getElementById("labelConfPassword")
+            let inputConfPass = document.getElementById("confPassword")
             e.preventDefault()
             data = {
                 User: e.target.user.value,
@@ -43,8 +79,31 @@ function registroUser() {
                 Password: e.target.password.value,
                 confPassword: e.target.confPassword.value
             }
-            addData(data);
-            form.reset();
+            readData(data.User)
+            if (data.Email === data.confEmail && data.Password === data.confPassword && data.User) {
+                addUser(data);
+                return false
+            }
+            if (data.Email !== data.confEmail) {
+                labelEmail.className = "error"
+                labelConfEmail.className = "error"
+                inputConfEmail.value = ""
+                inputConfEmail.placeholder = "Correo no coinciden"
+            } else {
+                labelEmail.className = ""
+                labelConfEmail.className = ""
+                inputConfEmail.placeholder = "Confirmar Correo"
+            }
+            if (data.Password !== data.confPassword) {
+                labelPassword.className = "error"
+                labelConfPassword.className = "error"
+                inputConfPass.value = ""
+                inputConfPass.placeholder = "Contraseña no coinciden"
+            } else{
+                labelPassword.className = ""
+                labelConfPassword.className = ""
+                inputConfPass.placeholder = "Confirmar Contraseña"
+            }
         })
     }
 }
