fab141bf0215f4fb334ba25e1890a3166d440f44
rubenleal89 <rubendarioleal618@gmail.com>
2023-04-01 12:05:43 -0500

    agregando comentarios en el codigo

--- logic.js
+++ logic.js
@@ -3,8 +3,10 @@ const indexedDB = window.indexedDB
 function txtBienvenida() {
     if (indexedDB) {
         let db
+        // Se crea la base de datos
         const req = indexedDB.open("Users",1)
 
+        // Se abre la base de datos ya creada
         req.onsuccess = ()=>{
             db = req.result
             console.log("OPEN",db);
@@ -23,11 +25,13 @@ function txtBienvenida() {
             })
         }
 
+        // Se lee el almacen de login para saber que tipo de usuario ingreso
         const readLogin = ()=>{
             const transaction = db.transaction(['LogIn'])
             const objectStore = transaction.objectStore('LogIn')
             const req = objectStore.openCursor()
     
+            // Si existe un usuario ya ingresado, ejecuta la funcion correspondiente
             req.onsuccess = (e)=>{ 
                 const cursor = e.target.result
                 if (cursor && cursor.value.Email == "admin") {
@@ -45,6 +49,7 @@ function txtBienvenida() {
             }
         }
 
+        // Si existe un usuario comun lee todos los usuario y lo compara con el ingresado
         const readUsers = (emailLogin)=>{
             const transaction = db.transaction(['signUp'])
             const objectStore = transaction.objectStore('signUp')
@@ -52,6 +57,7 @@ function txtBienvenida() {
             req.onsuccess = (e)=>{
                 const cursor = e.target.result
                 if (cursor) {
+                    // Comparacion de usuarios
                     if (emailLogin === cursor.value.Email) {
                         let ulMenu = document.getElementById("ulMenu")
                         ulMenu.innerHTML = ""
@@ -70,6 +76,7 @@ function txtBienvenida() {
                         const textoNameUser = document.getElementById("smsBienvenida")
                         textoNameUser.textContent ="Hola, "+ cursor.value.User
                     }
+                    // Cuando pase por el primer usuario, continue con el recorrido
                     cursor.continue()
                 }
                 
@@ -82,6 +89,7 @@ function txtBienvenida() {
 }
 
 function cerrarSesion(db) {
+    // Limpia toda la data los almacen logIn
     const transaction = db.transaction(['LogIn'],'readwrite')
     const objectStore = transaction.objectStore('LogIn')
     objectStore.clear()
@@ -91,7 +99,9 @@ function cerrarSesion(db) {
 function administracion(db) {
     let keyUser = ""
     let optionEjecutar = ""
+    // El (divListaUser)  es donde se imprime todos los usuario registrados
     let divListaUser = document.getElementById("listaUser")
+    // (addUser) es el boton para mostrar el form de agregar un usuario
     let addUser = document.getElementById("addUser")
     addUser.addEventListener("click",(e)=>{
         optionEjecutar = "AgregarUsuario"
@@ -99,11 +109,13 @@ function administracion(db) {
         formUpdate.className = ""
         formUpdate.reset()
     })
+    // (formUpdate) es el formulario para editar y agregar un usuario
     let formUpdate = document.getElementById("formEditar")
     formUpdate.addEventListener("submit",(e)=>{
         formUpdateData(e)
     })
     
+    // Funcion para imprimir los usuario en el (divListaUser)
     const printUsers = ()=>{
         const transaction = db.transaction(['signUp'], 'readwrite')
         const objectStore = transaction.objectStore('signUp')
@@ -112,6 +124,8 @@ function administracion(db) {
 
         req.onsuccess = (e)=>{
             const cursor = e.target.result
+            // Si existe algun usuario, se guarda la data en el objeto (dataUser)
+            // Para poder usarla en los eventos click y asi no perder la data en recorrido de la consulta
             if (cursor) {
                 let dataUser = {
                     key: cursor.key,
@@ -119,6 +133,7 @@ function administracion(db) {
                     email:cursor.value.Email,
                     password:cursor.value.Password
                 }
+                // Se crean los elementos que se van a imprimir con sus propiedades
                 let ulLista = document.createElement("ul")
                 ulLista.className = "navbar-nav col-lg-11 listaAdmin"
                 let liNameUser = document.createElement("li")
@@ -151,13 +166,16 @@ function administracion(db) {
                     updateDB()
                 })
 
+                // Se inserta el elemento ul (ulLista) en el div (divListaUser)
                 divListaUser.insertAdjacentElement("beforeend",ulLista)
+                // Se inserta el resto de elemetos en la lista ul (ulLista)
                 ulLista.insertAdjacentElement("beforeend",liNameUser)
                 ulLista.insertAdjacentElement("beforeend",liEmail)
                 ulLista.insertAdjacentElement("beforeend",liPassword)
                 ulLista.insertAdjacentElement("beforeend",btnEditar)
                 ulLista.insertAdjacentElement("beforeend",btnEliminar)
 
+                // Cuando pase por el primer usuario, continue con el recorrido
                 cursor.continue()
             }
         }
@@ -167,11 +185,13 @@ function administracion(db) {
         }
     }
 
+    // Al dar click en editar mostrara el formulario y al enviar se ejecutara esta funcion
     const updateDB = (dataForm)=>{
         const transaction = db.transaction(['signUp'], 'readwrite')
         const objectStore = transaction.objectStore('signUp')
         const req = objectStore.get(keyUser);
 
+        // se trae la data de ese usuario y se remplaza por la del formulario
         req.onsuccess = (e)=>{
             const data = e.target.result
             if (data && optionEjecutar === "Editar") {
@@ -181,14 +201,17 @@ function administracion(db) {
                 data.confEmail = dataForm.Email
                 data.confPassword = dataForm.Password
 
+                // Si todo esta bien, vuelve y ejecuta printUsers() y oculta el formulario
                 const updateRequest = objectStore.put(data);
                 updateRequest.onsuccess = (event) => {
                     formUpdate.className = "displayNone"
                     printUsers()
                 }
             }
+            // Si se da click en eliminar, se ejecutara esta parte del codigo
             else if(data && optionEjecutar === "Eliminar"){
                 const deleteRequest = objectStore.delete(keyUser);
+                // Si todo esta bien, vuelve y ejecuta printUsers()
                 deleteRequest.onsuccess = (event) => {
                     printUsers()
                 }
@@ -200,7 +223,10 @@ function administracion(db) {
         }
     }
 
+    // Si se desea agregar un nuevo usuario,
+    // mostrara el formulario y al enviar ejecutara esta funcion
     const addNewUser = (data)=>{
+        // Copio la data del formulario y le agrego los campos faltantes
         let dataNewUser = { ...data}
         dataNewUser.id = data.User+data.User
         dataNewUser.confEmail = data.Email
