3ee83c33f33702549dec3a44b7d39dd3f4a5d8d6
FrontedGamb <gamb02112004@gmail.com>
2023-05-05 14:12:27 -0500

    feat: ensambladora

---
+++ Components1.js
@@ -0,0 +1,90 @@
+const componentes = {
+    "Intel": [
+        {
+            id: 1,
+            "Board": [
+                "ASUS PRIME H510M-E",
+                "TUF GAMING B560M-E",
+                "GIGABYTE H510M S2H V2 (rev. 1.0)"
+            ],
+            "Procesadores": [
+                "i5-11600",
+                "I7-11700F",
+                "i9-10900F"
+            ],
+            "Fuente": [
+                "XPG pylon 550W 80 plus bronze",
+                "Cooler Master MWE 600W 80 Plus Bronze V2",
+                "Aerocool Cylon Rgb 600w 80 Plus Bronze"
+            ],
+            "Ram": [
+                "Corsair Vengeance RGB Pro 16GB DDR4 3200MHz",
+                "TEAMGROUP T-Force Delta RGB DDR4 16GB 3200MHz",
+                "PNY XLR8 Gaming 16GB DDR4 DRAM 3200MHz"
+            ],
+            "Almacenamiento": [
+                "WD_BLACK NVMe 500 GB Gen4 PCIe M.2 2280, hasta 4,000 MB/s ",
+                "1TB Crucial P5 Plus PCIe Gen4 3D NAND NVMe M.2 Gaming SSD, hasta 6600MB/s",
+                "SAMSUNG 980 PROM.2 NVMe Gen4 de 500 GB hasta 3500MB/s"
+            ],
+            "Case": [
+                "Cooler Master MasterBox MB500",
+                "Asus TUF GT301",
+                "Corsair 5000D Airflow"
+            ]
+        }
+    ],
+    "Ryzen": [
+        {
+            id: 2,
+            "Board": [
+                "MSI A520M PRO",
+                "MSI MPG B550 GAMING CARBON WIFI",
+                "MSI MEG X570S UNIFY-X MAX",
+                "MSI B450M MORTAR MAX",
+                "GIGABYTE A320M HD3 (rev. 1.0)",
+                "GIGABYTE GA-AX370M-Gaming 3 (rev. 1.x)",
+                "GIGABYTE B450M S2H V2 (rev. 1.x)"
+            ],
+            "Procesadores": [
+                "Ryzen 5 5600G",
+                "Ryzen 7 5700G",
+                "Ryzen 3 5300G",
+                "Athlon 3000G",
+                "Ryzen 3 2200G",
+                "Ryzen 5 2400G",
+                "Ryzen 7 4700G"
+            ],
+            "Fuente": [
+                "XPG pylon 650W 80 plus bronze",
+                "Evga 700w 700 bq 80 Plus Bonze",
+                "500w Thermaltake Smart 80 Plus White",
+                "Giga-Byte Technology P650B 650W",
+                "Cougar Vtc600 600w 80 + White",
+                "Cooler Master 600w Elite V3",
+                "Azza PSAZ-650W"
+            ],
+            "Ram": [
+                "Teamgroup T-Force Vulcan TUF Gaming DDR4 16GB 3200MHz",
+                "Corsair Vengeance LPX 16GB DDR4 3200MHz",
+                "Silicon Power Value Gaming DDR4 RAM 16GB 3200MHz",
+                "G.Skill Trident Z RGB 32GB DDR4 3600MHz",
+                "Crucial Ballistix RGB 16GB DDR4 3600MHz",
+                "Kingston HyperX Fury RGB DDR4 16GB 3200MHz",
+                "Patriot Viper Steel DDR4 16GB 4400MHz",
+                "ADATA XPG Spectrix D60G DDR4 16GB 3600MHz"
+            ],
+            "Storage": [
+                "Samsung 970 EVO Plus 1TB NVMe SSD",
+                "Western Digital Black SN750 1TB NVMe SSD",
+                "Crucial MX500 1TB 3D NAND SATA 2.5 Inch Internal SSD",
+                "Seagate BarraCuda 4TB Internal Hard Drive HDD",
+                "ADATA SU635 960GB 3D NAND SSD",
+                "Kingston A400 SSD 480GB SATA 3 2.5” Solid State Drive",
+                "Toshiba Canvio Advance 4TB Portable External Hard Drive",
+                "Sabrent Rocket Q4 NVMe PCIe 4.0 M.2 1TB SSD"
+            ]
+        }
+        ]
+    }
+
