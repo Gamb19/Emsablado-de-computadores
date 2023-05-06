45abf4f2d74e5f9eef78099e750af98be5f5705c
rubenleal89 <rubendarioleal618@gmail.com>
2023-05-06 12:33:56 -0500

    Data de components.js subida a la BD y adaptacion de la data a los select de constructora.html

--- Js/constructora.js
+++ Js/constructora.js
@@ -19,7 +19,7 @@ if (indexedDB) {
         req.onsuccess = (e)=>{
             const cursor = e.target.result
             if (cursor) {
-                printComponents(cursor.value)
+                printData(cursor.value)
             }
         }
         req.onerror = (e) =>{
@@ -28,12 +28,58 @@ if (indexedDB) {
     }
 }
 
-function printComponents(data) {
-
+function printData(data) {
     const { Intel, Ryzen } = data
+    
 
-    let containerComp = document.getElementById('containerComponents')
-    Intel.forEach(element => {
-        console.log(element)
+    let selectMarca = document.getElementById('select-marca')
+    selectMarca.addEventListener('change', (e)=>{
+
+        if (e.target.value === 'Intel') {
+            let data = Object.entries(Intel)
+            printOptions(data)
+        }
+        else {
+            const data = Object.entries(Ryzen)
+            printOptions(data)
+        }
+
+    })
+}
+
+function printOptions(data) {
+
+    // El nombre de las (key) de cada (marca) debe ser igual al (id) de cada (select)
+    // La separacion de cada palabra de un id o key tiene que ser con un ( _ )
+    /* MarcaIntel: {
+        key1:"",
+        key2:""
+        }
+    */
+
+    data.forEach(element => {
+
+        // element[0] -> son las key de cada marca
+        // algunas key viene separadas con ( _ )
+        // element[1] -> Es la data de cada option (key)
+        
+        let select = document.getElementById(`${element[0]}`)
+        select.innerHTML = '' //Se limpia los selects
+        let option = document.createElement('option') // Se crean denuevo las opciones principales
+        option.setAttribute = 'selected'
+        option.textContent = element[0].replace(RegExp(/_/g),' ') // Remplazamos los ( _ ) por espacios en blanco
+        select.insertAdjacentElement('beforeend',option)
+
+        // console.log(element[0].replace(RegExp(/_/g),' ')
+
+        element[1].forEach(nameOption => {
+            // Se crean las opciones de cada select
+            let option = document.createElement('option')
+            option.value = nameOption
+            option.textContent = nameOption
+            select.insertAdjacentElement('beforeend',option)
+        })
+
     });
+
 }
